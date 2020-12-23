fc99723e2a961138d7ef32bdaf9f05feaeb079fd
root <your_email@example.com>
2020-12-23 20:54:55 +0300

    test commit in new branch

--- resources/views/tmp/base.blade.php
+++ resources/views/tmp/base.blade.php
@@ -29,7 +29,7 @@
         @yield('content')
         @yield ('chat')
     </div>
-
+    {{-- qwe --}}
     <!-- ADD AJAX-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
     <!-- ADD JS -->
